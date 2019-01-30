49dafb50781fce49f70dc66d1993b5847660ead3
skino0 <skinogigo@gmail.com>
2019-01-30 18:52:26 +0200

    currently jquery scripts

---
+++ js/nav.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	/*$('#toggle').hover(function(){
+		$('#line1').addClass('en1');
+		$('#line2').addClass('en2');
+
+	});
+
+	$('#toggle').mouseout(function(){
+		$('#line1').removeClass('en1');
+		$('#line2').removeClass('en2');
+
+	});*/
+
+		$('#toggle').click(function(){
+		$('#line1').addClass('en1');
+		$('#line2').addClass('en2');
+		$('#line3').addClass('en3');
+	});
+
+
+});
