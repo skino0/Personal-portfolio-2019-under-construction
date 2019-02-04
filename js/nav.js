e4c05d86837c24e397a72930f86286b08d25dee7
skino0 <skinogigo@gmail.com>
2019-02-04 16:54:59 +0200

    Removed almost all elements that are not so nice

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,5 @@
 $(document).ready(function(){
 	
-		$('#toggle').click(function(){
-		$('#line1').addClass('en1');
-		$('#line2').addClass('en2');
-		$('#line3').addClass('en3');
-		$(this).addClass('clicked');
-		$('#nav').addClass('visible');
-	});
+
 
 });
