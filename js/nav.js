a46c030ced0c97f60b8e01591f4e6835546576b3
skino0 <skinogigo@gmail.com>
2019-02-05 20:19:35 +0200

    removed top border from mail style it better

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $('#contact').click(function(){
         $('#papaki').css('transition-duration','1s').css('color','#ff7675');
         $('#mail').css('transition-duration','.5s').css('color','#81ecec').
-        css('padding','15px 0px 15px 0px').css('border-top','1px rgba(255,255,255,.8) solid').css('border-bottom','1px rgba(255,255,255,.8) solid');
+        css('padding','0px 0px 15px 0px').css('border-bottom','1px #ff7675 solid');
         $(this).css('display','none');    
 
     });
