3564eafe405d94e69a69b410096d6fd6aa26c276
skino0 <skinogigo@gmail.com>
2019-02-07 20:18:21 +0200

    end with animation scripts

--- js/projects.js
+++ js/projects.js
@@ -1,12 +1,30 @@
 $('.project:eq( 0 )').click(function(){
     $('.folder:eq( 0 )').addClass('change');
+    $('.link:eq( 0 )').addClass('op');
+
 });
 $('.project:eq( 1 )').click(function(){
     $('.folder:eq( 1 )').addClass('change');
+    $('.link:eq( 1 )').addClass('op');
 });
 $('.project:eq( 2 )').click(function(){
     $('.folder:eq( 2 )').addClass('change');
+    $('.link:eq( 2 )').addClass('op');
 });
 $('.project:eq( 3 )').click(function(){
     $('.folder:eq( 3 )').addClass('change');
+    $('.link:eq( 3 )').addClass('op');
+});
+$('.project:eq( 4 )').click(function(){
+    $('.folder:eq( 4 )').addClass('change');
+    $('.link:eq( 4 )').addClass('op');
+});
+$('.project:eq( 5 )').click(function(){
+    $('.folder:eq( 5 )').addClass('change');
+    $('.link:eq( 5 )').addClass('op');
+});
+$('.project:eq( 6 )').click(function(){
+                $('.folder:eq( 6 )').addClass('change');
+    $('.link:eq( 6 )').addClass('op');
+
 });
