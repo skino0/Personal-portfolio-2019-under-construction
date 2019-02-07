8310b28ea61c51731a7e72df38dc61236aa71fb1
skino0 <skinogigo@gmail.com>
2019-02-07 19:27:19 +0200

    created projects.js

---
+++ js/projects.js
@@ -0,0 +1,12 @@
+$('.project:eq( 0 )').click(function(){
+    $('.folder:eq( 0 )').addClass('change');
+});
+$('.project:eq( 1 )').click(function(){
+    $('.folder:eq( 1 )').addClass('change');
+});
+$('.project:eq( 2 )').click(function(){
+    $('.folder:eq( 2 )').addClass('change');
+});
+$('.project:eq( 3 )').click(function(){
+    $('.folder:eq( 3 )').addClass('change');
+});
