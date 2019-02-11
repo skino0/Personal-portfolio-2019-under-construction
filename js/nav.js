fb0624d5c5d5adcc2a1c6337d51cdaa94206f1d8
skino0 <skinogigo@gmail.com>
2019-02-11 18:12:09 +0200

    start mobile version

--- js/nav.js
+++ js/nav.js
@@ -5,13 +5,15 @@ $(document).ready(function(){
         css('padding','0px 0px 15px 0px').css('border-bottom','1px #ff7675 solid');
         $(this).css('display','none');    
     });
-    
+
     $('#work').click(function(){
         $('#aboutme').addClass('hiding');
         $('#projects').addClass('visible');
         $('#github').addClass('vsb');
         $('#codepen').addClass('vsbl');
         $('#text').addClass('hide');
+        $('#ski').attr('src','images/megif.gif');
         $('.li:first-child').remove();
+        $('svg').remove();
     });
 }); 
